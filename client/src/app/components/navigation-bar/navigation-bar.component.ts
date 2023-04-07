7cacfad13df7bed811433f88b034b577efb4d6b3
KiKero99 <74576377+KiKero99@users.noreply.github.com>
2023-04-07 13:06:45 -0400

    [add] header links

--- client/src/app/components/navigation-bar/navigation-bar.component.ts
+++ client/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent {
+  public route: string;
 
+  public constructor(location: Location, router: Router) {
+    router.events.subscribe((_val: any) => {
+      if (location.path() !== "") {
+        this.route = location.path();
+      } else {
+        this.route = "";
+      }
+    });
+  }
 }
