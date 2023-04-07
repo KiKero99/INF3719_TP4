c8c81dcd60f75993ec0fc0e407e671aa9b260b47
KiKero99 <74576377+KiKero99@users.noreply.github.com>
2023-04-07 12:27:54 -0400

    [add] paths to new pages

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { MembersPageComponent } from '../pages/members-page/members-page.component';
+import { ReservationsPageComponent } from "../pages/reservations-page/reservations-page.component";
+import { ReservationsCreationPageComponent } from "../pages/reservations-creation-page/reservations-creation-page.component";
 
 import { AppComponent } from "../app.component";
 
 const routes: Routes = [
-  { path: "app", component: AppComponent }
+  { path: "app", component: AppComponent },
+  { path: "members", component: MembersPageComponent },
+  { path: "reservations", component: ReservationsPageComponent },
+  { path: "creation", component: ReservationsCreationPageComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
